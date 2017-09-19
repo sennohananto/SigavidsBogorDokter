0a8c21a82ca5581d7c34d26a41f76e2ecda29ea2
sennohananto <sennohananto@gmail.com>
2017-09-19 15:42:22 +0700

    Splash Activity Ditambahkan

---
+++ app/src/main/java/besidev/sigavidsbogor/SplashActivity.java
@@ -0,0 +1,51 @@
+package besidev.sigavidsbogor;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+    private Thread splashTread;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    // Splash screen pause time
+                    while (waited < 2000) {
+                        sleep(100);
+                        waited += 100;
+                    }
+//                    Context context = SplashActivity.this;
+//                    SharedPreferences sharedPref = context.getSharedPreferences("dataLogin",context.MODE_PRIVATE);
+//                    if(sharedPref.getString("nim",null) != null){
+//                        Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+//                        intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+//                        startActivity(intent);
+//                    }else{
+//                        Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+//                        intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+//                        startActivity(intent);
+//                    }
+
+                    Intent keLoginActivity = new Intent(SplashActivity.this,LoginActivity.class);
+                    startActivity(keLoginActivity);
+                    SplashActivity.this.finish();
+                } catch (InterruptedException e) {
+                    // do nothing
+                } finally {
+                    SplashActivity.this.finish();
+                }
+            }
+        };
+        splashTread.start();
+    }
+}
